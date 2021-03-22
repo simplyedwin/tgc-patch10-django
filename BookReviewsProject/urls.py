1a0d26d0c41d16161fcf077c7389e913b2c105a3
simplyedwin <simplyedwin@gmail.com>
2021-03-22 08:54:05 +0000

    added reviews app

--- BookReviewsProject/urls.py
+++ BookReviewsProject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import books.views
+import reviews.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', books.views.index),
+    path('reviews/', reviews.views.index)
 ]
