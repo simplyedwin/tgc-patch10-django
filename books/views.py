0c6b7ef7aa59b806379e8ee88fb7cb5537a6418d
simplyedwin <simplyedwin@gmail.com>
2021-03-22 09:51:26 +0000

    updated reviews app

--- books/views.py
+++ books/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("Books app")
+    return render(request, 'books/index-template.html')
--- reviews/views.py
+++ reviews/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to reviews")
+    return render(request, "reviews/index-template.html")
